7d0e7ef0a315296a9d8c07a5fd6cf9ca66726ce9
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-12-24 18:54:33 -0600

    Update NodeRED_UI_Fix.user.js

--- NodeRED_UI_Fix.user.js
+++ NodeRED_UI_Fix.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Node-RED UI Fix
-// @homepageURL https://github.com/AlgoClaw/UImods/blob/main/NodeRED_UI_Fix.user.js
-// @downloadURL https://raw.githubusercontent.com/AlgoClaw/UImods/main/NodeRED_UI_Fix.user.js
-// @updateURL   https://raw.githubusercontent.com/AlgoClaw/UImods/main/NodeRED_UI_Fix.user.js
+// @homepageURL https://github.com/AlgoClaw/UImods/blob/main/Scrutiny_UI_Fix.user.js
+// @downloadURL https://raw.githubusercontent.com/AlgoClaw/UImods/main/Scrutiny_UI_Fix.user.js
+// @updateURL   https://raw.githubusercontent.com/AlgoClaw/UImods/main/Scrutiny_UI_Fix.user.js
 // @include     *://*:1880/*
 // @description null
 // @version     0.001
@@ -36,23 +36,32 @@ var CSSMod = /** @class */ (function () {
     //
     CSSMod.prototype.applyCSSFix = function (css) {
         //
-        //
+        css += "[id=red-ui-workspace] {display: flex !important;}\n"; //
+        css += "[id=red-ui-workspace] {flex-wrap: wrap !important;}\n"; //
         css += ".red-ui-tab-scroll {display: none !important;}\n"; //
-        css += ".red-ui-tabs {height: 100% !important;}\n"; //
+        css += ".red-ui-tabs {height: fit-content !important;}\n"; //
         css += ".red-ui-tabs {padding-left: 0px !important;}\n"; //
-        css += ".red-ui-tabs {padding-right: unset !important;}\n"; //
-        css += ".red-ui-tabs-scroll-container {height: 100% !important;}\n"; //
+        css += ".red-ui-tabs {margin-bottom: 0px !important;}\n"; //
+        css += ".red-ui-tabs {overflow: unset !important;}\n"; //
+        css += ".red-ui-tabs {padding-right: 0px !important;}\n"; //
+        css += ".red-ui-tab-button.red-ui-tabs-add {display: none !important;}\n"; //
+        css += ".red-ui-tab-button.red-ui-tabs-menu {display: none !important;}\n"; //
+        css += ".red-ui-tabs-scroll-container {height: fit-content !important;}\n"; //
+        css += ".red-ui-tabs-scroll-container {overflow-x: unset !important;}\n"; //
+        css += ".red-ui-tabs-scroll-container {overflow-y: unset !important;}\n"; //
         css += "[id=red-ui-workspace-tabs] {width: 100% !important;}\n"; //
         css += "[id=red-ui-workspace-tabs] {opacity: unset !important;}\n"; //
         css += "[id=red-ui-workspace-tabs] {display: flex !important;}\n"; //
         css += "[id=red-ui-workspace-tabs] {flex-wrap: wrap !important;}\n"; //
+        css += "[id=red-ui-workspace-tabs] {height: fit-content !important;}\n"; //
         css += ".red-ui-tab {margin-left: 1px !important;}\n"; //
         css += ".red-ui-tab {margin-right: 1px !important;}\n"; //
         css += ".red-ui-tab {width: unset !important;}\n"; //
         css += ".red-ui-tab-label {padding-left: 1px !important;}\n"; //
         css += ".red-ui-tab-label {padding-right: 1px !important;}\n"; //
         css += ".red-ui-tabs-fade {display: none !important;}\n"; //
-        css += "[id=red-ui-workspace-chart] {margin-top: 50px !important;}\n"; //
+        css += "[id=red-ui-workspace-chart] {top: unset !important;}\n"; //
+        css += "[id=red-ui-workspace-chart] {position: unset !important;}\n"; //
         //
         return css;
     };
