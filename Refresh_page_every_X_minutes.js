8ddacc9498a6af91a5e5701937a01eb34976a147
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-11-14 15:57:35 -0600

    Update Refresh_page_every_X_minutes.js

--- Refresh_page_every_X_minutes.js
+++ Refresh_page_every_X_minutes.js
@@ -1,13 +1,12 @@
-    // ==UserScript==
-    // @name         Refresh_page_every_X_minutes
-    // @version      0.0
-    // @description  null
-    // @match        https://patentcenter.uspto.gov/*
-    // @grant        none
-    // ==/UserScript==
+// ==UserScript==
+// @name         Refresh_page_every_X_minutes
+// @version      0.0
+// @description  null
+// @match        https://patentcenter.uspto.gov/*
+// @grant        none
+// ==/UserScript==
 
-    (function() {
-        'use strict';
-        setTimeout(function(){ location.reload(); }, 5*60*1000);
-    })();
-
+(function() {
+    'use strict';
+    setTimeout(function(){ location.reload(); }, 5*60*1000);
+})();
